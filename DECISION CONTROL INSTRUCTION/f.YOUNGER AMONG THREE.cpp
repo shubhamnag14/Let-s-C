dbe523072af5dcbddd08013c0de11d4d25e5eba0
Shubham Nag <43213463+shubhamnag14@users.noreply.github.com>
2021-03-15 01:26:53 +0530

    Add files via upload

---
+++ DECISION CONTROL INSTRUCTION/f.YOUNGER AMONG THREE.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+main()
+{
+	int x, y, z;
+	printf("Enter the age of RAM:");
+	scanf("%d",&x);
+	printf("Enter the age SHYAM:");
+	scanf("%d",&y);
+	printf("Enter the age of AJAY:");
+	scanf("%d",&z);
+	if(x<y)
+	{
+		if(x<z)
+		{
+			printf("RAM IS YOUNGER");
+		}
+	}
+	if (y<x)
+	{
+		if (y<z)
+		{
+			printf("SHYAM IS YOUNGER");
+		}
+	}
+	if(z<x)
+	{
+		if (z,y)
+		{
+			printf("AJAY IS YOUNGER");
+		}
+	}
+}
