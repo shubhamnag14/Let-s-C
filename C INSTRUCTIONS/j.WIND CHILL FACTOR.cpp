f78301078be82b357c60093b39611fd11bbe30b5
Shubham Nag <43213463+shubhamnag14@users.noreply.github.com>
2021-03-17 23:45:37 +0530

    Update j.WIND CHILL FACTOR.cpp

--- C INSTRUCTIONS/j.WIND CHILL FACTOR.cpp
+++ C INSTRUCTIONS/j.WIND CHILL FACTOR.cpp
@@ -5,6 +5,6 @@ main()
 	float temp,vel,wcf;
 	printf("Enter values of temp in velocity:");
 	scanf("%f%f",&temp,&vel);
-	wcf = 35.74 + 0.6215*temp+(0.4275*t-35.75)*pow(vel,0.16f); // wind chilling factor formule
+	wcf = 35.74 + 0.6215*temp+(0.4275*temp-35.75)*pow(vel,0.16f); // wind chilling factor formule
 	printf("WIND CHILL FACTOR = %f\n",wcf);
 }
